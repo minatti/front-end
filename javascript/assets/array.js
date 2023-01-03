73d2ab78b56dcf917640cf91cd839fd310dead46
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-03 06:53:19 -0300

    array(matriz)

---
+++ javascript/assets/array.js
@@ -0,0 +1,16 @@
+//array dentro de array (formando duas dimensões)
+let lista = ['blabla', ['xx', 'yy']];
+
+
+let listaIdent = [
+    'bla',
+    [
+    'xx', 
+    'yy'
+    ]
+    ,
+    ['abc'
+    ,
+     'def'
+    ]
+];
