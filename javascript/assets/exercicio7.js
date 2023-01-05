2bf910e2d4bdf4b96489028181b6f56fc50e417a
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-04 21:06:52 -0300

    exercicio resolvido

---
+++ javascript/assets/exercicio7.js
@@ -0,0 +1,27 @@
+// 1. No array abaixo, qual o número que pega a Ferrari?
+// Número 1
+
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let indice = 1
+
+
+console.log(`busca na lista: ${carros[indice]}`);
+
+
+
+// 2. Troque a Ferrari por Audi
+
+carros[1] = 'Audi'
+
+
+
+console.log(`2 Lista atualizada com Audi: ${carros}`);
+
+// 3; Adicione o Volvo a lista
+carros.push('Volvo');
+
+console.log(carros)
+
+
+console.log(`4. itens no array: ${carros.length}`);
+
