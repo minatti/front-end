2670e2725e5ce96ec05b2f4acf86af2f742ea499
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-07 15:34:56 -0300

    iteracao de arrays

---
+++ javascript/assets/iteracaoArrays.js
@@ -0,0 +1,13 @@
+// iteracao arrays
+let fruits = ['Banana', 'Laranja', 'Maça', 'Uva'];
+
+
+let ok = fruits.every(()=>{
+    return value.length > 3;
+});
+
+
+includes()// substitui tem que percorrer uma lista para retornar 0/1
+filter()//busca
+every()// todos satisfatorios
+some() // apenas 1 resultado for satisfatorios
