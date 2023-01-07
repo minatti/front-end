08741fa3e103e6d6fccf18e142afd82d7e45b1ab
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-07 10:45:54 -0300

    exemplo while

---
+++ javascript/assets/loopWhile.js
@@ -0,0 +1,31 @@
+// while = enquanto
+// funciona como uma repetição com condicional ou seja enquanto sua condição for true, executa...
+// ex prático
+
+
+let contador = 0;
+
+while (contador <= 10) {
+    console.log(`O número da vez é: ${contador}`)
+}
+
+
+// tem casos que digamos que tu precisa ficar checkando uma determinada informação 
+// para deixar sua variavel boolean true or false
+// 
+
+
+// let isActive = true;
+let isActive = false;
+
+
+while(isActive){
+    
+    // seila to atualizando uma informação em outro sistema
+    // 1x
+    // identifico a informação: atualizou? nao, blz
+    // 2x
+    // atualizou
+    // entao mudo o estado de isActive;
+
+}
