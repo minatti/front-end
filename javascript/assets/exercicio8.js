3dbbe48a3bfa46dbc0a6d3f9109ffabb724eb2a7
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-07 12:52:36 -0300

    resolvido

---
+++ javascript/assets/exercicio8.js
@@ -0,0 +1,22 @@
+// 1. Faça um loop que mostre todas as frutas;
+
+let fruits = ['Maça', 'Uva', 'Banana'];
+
+
+
+for(let i in fruits){
+    console.log(`Frutas-->${fruits[i]}`);
+
+}
+
+
+//2. Conte de 1 até 100 atráves de um While
+let numero = 1;
+
+while(numero < 101){
+    
+    console.log(`Numero: ${numero}`);
+    numero++;
+    
+    
+}
--- javascript/assets/loopWhile.js
+++ javascript/assets/loopWhile.js
@@ -10,22 +10,3 @@ while (contador <= 10) {
 }
 
 
-// tem casos que digamos que tu precisa ficar checkando uma determinada informação 
-// para deixar sua variavel boolean true or false
-// 
-
-
-// let isActive = true;
-let isActive = false;
-
-
-while(isActive){
-    
-    // seila to atualizando uma informação em outro sistema
-    // 1x
-    // identifico a informação: atualizou? nao, blz
-    // 2x
-    // atualizou
-    // entao mudo o estado de isActive;
-
-}
