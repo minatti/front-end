997d8cc307874b8c77bac0b1f25e40114c8af3ef
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-07 16:05:52 -0300

    como acessar elementos

---
+++ javascript/assets/selElemento.js
@@ -0,0 +1,14 @@
+//Identificar o elemento para manipulação.
+// Window / Document
+
+getElementsByID(); 
+getElementsByTagName(); //retorna tipo array
+getElementsClassName(); //retorna tipo array
+document.querySelector("#teste"); //css
+document.querySelectorAll("#teste ul li");
+resultado = document.getElementById("teste");
+
+console.log(resultado);
+
+
+
