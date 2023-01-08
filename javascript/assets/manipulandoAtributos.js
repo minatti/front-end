186442371b0476e2c98eb3cd1a727c4a0b908387
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-08 11:18:56 -0300

    manipulando atribs

---
+++ javascript/assets/manipulandoAtributos.js
@@ -0,0 +1,14 @@
+function clicou(){
+   const input = document.querySelector('input');
+   const botao = document.querySelector('.botao');
+
+    if(input.getAttribute('type') === 'text') {
+        input.setAttribute('type', 'password');
+        botao.innerText = "Mostrar senha";
+    } else {
+        input.setAttribute('type', 'text');
+        botao.innerText = "Ocultar senha";
+    }
+
+
+}
