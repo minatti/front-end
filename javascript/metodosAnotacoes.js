6770f8a2a056300c7358456554a53b5266a2fe30
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-14 11:36:22 -0300

    metodos strings/numbers

---
+++ javascript/metodosAnotacoes.js
@@ -0,0 +1,18 @@
+// Metodos de Strings
+
+indexOf(posIni); // se nao encontrar a str, retorna o valor de -1;
+slice(posIni, posFim);
+replace(strAtual, novaStr);
+// Aplicando conceito de CamelCase
+toUpperCase(maiusculo);
+toLowerCase(minusculo);
+trim(removeEspacos,esquerda/direita);
+split(sutilArteDeFatiarStrApartirDeumCaractereChave);
+
+
+// Metodos de Números
+
+toString(); //numero para texto
+toFixed(2); // formata para moeda
+parseInt(); // transformar em inteiro
+parseFloat(); // transformar em float para números decimais
