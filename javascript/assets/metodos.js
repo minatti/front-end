67fe56a94751c221cee8b4466e8f0ee190fe118e
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-16 12:54:26 -0300

    metodos de strings/numbers/format

---
+++ javascript/assets/metodos.js
@@ -0,0 +1,32 @@
+// Metodos de Strings
+
+indexOf(posIni); // se nao encontrar a str, retorna o valor de -1;
+slice(posIni, posFim);
+replace(strAtual, novaStr);
+// Aplicando conceito de CamelCase
+toUpperCase(maiusculo);
+toLowerCase(minusculo);
+trim(removeEspacos,esquerda/direita);
+split(sutilArteDeFatiarStrApartirDeumCaractereChave);
+
+
+// Metodos de Números
+
+toString(); //numero para texto
+toFixed(2); // formata para moeda
+parseInt(); // transformar em inteiro
+parseFloat(); // transformar em float para números decimais
+
+
+// Metodos array
+
+pop(); // remove ultimo item 
+shift(); // remove primeiro item
+push(); // add item
+filter(); // filtra os valores a partir do parametro (valor chave)
+find(); //a prim condicao satisfatoria
+findIndex(); // retorna o num posicao do ind do array
+
+
+// combina isso com estrutra de controle que já fica bem interessante para resolver problemas
+
