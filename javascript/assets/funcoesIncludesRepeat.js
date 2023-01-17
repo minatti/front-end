859b7620247f82e28856760502b49b527c3d192e
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-17 18:21:59 -0300

    funcoes para te ajudar includes e repeat

---
+++ javascript/assets/funcoesIncludesRepeat.js
@@ -0,0 +1,9 @@
+// includes
+let lista = ['cafe', 'arroz', 'feijão', 'macarrão'];
+
+console.log(lista.includes('carne'));
+
+//retorna true or false;
+
+// Util para teste e prototipações
+console.log('Fulaninho'.repeat(120));
