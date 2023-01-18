8a2a465c8d147dc483b4686c3b467730204bcd8d
Luan Carminatti <luancarminatt@gmail.com>
2023-01-18 16:55:11 -0300

    update with anonymous function and test with param

--- javascript/assets/funcoesStrings.js
+++ javascript/assets/funcoesStrings.js
@@ -1,15 +1,15 @@
-const nroContratoPadrao = () => {
-    let qtdDigitos = 10;
-    let nroContrato = 89879778;
-    let verificarQtd = nroContrato.toString().length;
-    // console.log(verificarQtd);
-    let qtdNumerica = parseInt(verificarQtd);
-    console.log(`TOTAL DE DIGITO: ${qtdDigitos} ----- ADICIONAR X DIGITOS: ${qtdNumerica}`)
-    var totDigits = qtdDigitos-qtdNumerica;
-    console.log(totDigits);
-    let resultado = totDigits;
-    console.log(`RESULTADO: ${resultado}`);
-    return nroContrato.padStart(2, '0');
+let value = '124';
+
+const normalizeContract = (value) => {
+    //let contract = '89879778';
+    contract = value;
+    const qtdPattern = (10-contract.length)+contract.length;
+    console.log(qtdPattern);
+    //const last4Digits = fullNumber.slice(-4);
+    const addDigit = contract.padStart(qtdPattern, '0');
+
+    console.log(addDigit);
+    return addDigit;
 }
 
-console.log(nroContratoPadrao());
+console.log(normalizeContract(value));
