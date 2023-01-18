4b2b73ee4a07ec5f29570661cb3ecca62b1a1c01
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-18 17:15:58 -0300

    requisitando para doc tipo json

---
+++ javascript/assets/request.js
@@ -0,0 +1,19 @@
+
+const request = () => {
+    let url = 'https://jsonplaceholder.typicode.com/posts';
+
+    fetch(url).then((response) => {
+        return response.json();
+        
+    })
+    .then((json) => {
+        alert(`Titulo do primeiro post: ${json[0].title}`);
+    })  
+
+}
+
+const sendCallback = () => {
+    return document.querySelector('#button').addEventListener('click', request);
+}
+
+console.log(sendCallback());
