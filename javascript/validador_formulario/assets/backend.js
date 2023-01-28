db388767dd2dc778bf8990721e38b23c4a50b068
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-01-28 14:49:48 -0300

    validator para formulario

---
+++ javascript/validador_formulario/assets/backend.js
@@ -0,0 +1,71 @@
+let validator = {
+
+    handleSubmit:(event) => {
+        event.preventDefault();
+        let send = true;
+        let inputs = form.querySelectorAll('input');
+
+        validator.clearErrors();
+
+        for(let i=0; i < inputs.length; i++) {
+            let input = inputs[i];
+            let check = validator.checkInput(input);
+
+            if(check !== true) {
+                send=false;
+                validator.showError(input, error);
+                //console.log(check);
+            }
+
+        }
+        if(send) {
+            form.submit();
+        }                
+    },
+    checkInput:(input) => {
+        let rules = input.getAttribute('data-rules');
+        if(rules !== null) {
+            rules = rules.split('|');
+            for(let k in rules) {
+                let rDetails = rules[k].split('=');
+                let txtEmpty = 'Campo não pode ser vazio.';
+                switch(rDetails[0]){
+                    case 'required':
+                        if(input.value === '') {
+                            return txtEmpty;
+
+                        }
+                    break;
+                    case 'min':
+                    break;        
+                }
+            }
+
+        }
+        return true;   
+    },
+    showError:(input, error) => {
+        input.style.borderColor = '#FF0000';
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+        for(let i=0;i<inputs.length;i++) {
+            inputs[i].style = '';
+        }
+        let errorElements = document.querySelectorAll('.error');
+        for(let i=0;i<errorElements.length;i++) {
+            errorElements[i].remove();
+        }
+    }
+
+};
+    
+
+let form = document.querySelector('.validator');
+form.addEventListener('submit', validator.handleSubmit);
+
