d902512f96a2815a6fbd3f226c0e30c8d313f716
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-02-13 22:13:58 -0300

    incluindo informacoes de etapas

---
+++ javascript/urna_eletronica/etapas.js
@@ -0,0 +1,33 @@
+let etapas = [
+    {
+    
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+            numero: 38111,
+            name: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos: 
+                [{
+                    url: '38111.jpg', legenda: 'Vereador', small: false
+                }]
+            },
+            {
+                numero: 77222,
+                name: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [{
+                    url:'77222.jpg', legenda: 'Vereador'
+                }]
+            },
+            ]
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            
+        ]
+    }
+    ]
---
+++ javascript/urna_eletronica/script.js
@@ -0,0 +1,7 @@
+voto = document.querySelector('.d-1-1 span');
+vereador = document.querySelector('.d-1-2 span');
+iterarNumbers = document.querySelectorAll('.d-1-3');
+
+
+console.log(voto);
+console.log(vereador);
