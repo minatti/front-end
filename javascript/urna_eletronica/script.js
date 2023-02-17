04e22b302a76a90df4759ce04bebf5c64b2249fb
Luan Carminatti <luan_carminatti@REDEBRASIL.LOCAL>
2023-02-17 13:35:22 -0300

    controle das areas

--- javascript/urna_eletronica/script.js
+++ javascript/urna_eletronica/script.js
@@ -1,7 +1,6 @@
 voto = document.querySelector('.d-1-1 span');
-vereador = document.querySelector('.d-1-2 span');
-iterarNumbers = document.querySelectorAll('.d-1-3');
-
-
-console.log(voto);
-console.log(vereador);
+cargo = document.querySelector('.d-1-2 span');
+iterarNumbers = document.querySelector('.d-1-3');
+descricao = document.querySelector('.d-1-4');
+aviso = document.querySelector('.d-2');
+lateral = document.querySelector('.d-1-right');
